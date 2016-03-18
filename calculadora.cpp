4c110b4aa65c104ba64bc9626e27af1cd6dcea48
oadmng <oadmng@gmail.com>
2016-03-18 00:13:43 -0500

    Update calculadora.cpp

--- calculadora.cpp
+++ calculadora.cpp
@@ -12,6 +12,8 @@ int multiplicar(int a, int b, int mod){
     return ((a% mod)*(b%mod))%mod;
     }
 int dividir(int a, int b, int mod){
+	if(b==0)
+		{cout<<"La division entre 0 no es posible"<<endl;return -1;}
     return ((a% mod)/(b%mod))%mod;
     }
 
@@ -36,26 +38,30 @@ while(salida != 0){
 	menu();
 	int opcion;
 	cin>>opcion;
-  int a;//primer numero
-  int b;//segundo numero
-  int modulo;
-  int respuesta=0;
-  cout<<"Ingrese 1er numero "<<endl;
-  cin>>a;
-  cout<<"Ingrese 2do numero "<<endl;
-  cin>>b;
-  cout<<"Ingrese modulo con el que trabajaremos "<<endl;
-  cin>>modulo;
-  if(opcion==1){
-      respuesta=suma(a,b,modulo);}
-  if(opcion==2){
-      respuesta=resta(a,b,modulo);}
-  if(opcion==3){
-      respuesta=multiplicar(a,b,modulo);}
-  if(opcion==4){
-      respuesta=dividir(a,b,modulo);}
-  cout<<endl<<"La respuesta es :"<<respuesta<<endl;
-  cout<<endl<<"Que desea hacer? 1: seguir       0:salir"<<endl;
+    int a;
+    int b;
+    int modulo;
+    int respuesta=0;
+    cout<<"Ingrese 1er numero "<<endl;
+    cin>>a;
+    cout<<"Ingrese 2do numero "<<endl;
+    cin>>b;
+    cout<<"Ingrese modulo con el que trabajaremos "<<endl;
+    cin>>modulo;
+    if(opcion==1){
+        respuesta=suma(a,b,modulo);}
+    if(opcion==2){
+        respuesta=resta(a,b,modulo);}
+    if(opcion==3){
+        respuesta=multiplicar(a,b,modulo);}
+    if(opcion==4){
+        respuesta=dividir(a,b,modulo);
+		}
+	if(respuesta != -1){
+		cout<<endl<<"La respuesta es :"<<respuesta<<endl;
+	}	
+    
+    cout<<endl<<"Que desea hacer? 1: seguir       0:salir"<<endl;
 	cin>> salida;
 	}
 }
