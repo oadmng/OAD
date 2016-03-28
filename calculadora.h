b29ff0933e96f725b12cf372a5d5a2cf78a9893e
oadmng <oadmng@gmail.com>
2016-03-27 20:28:09 -0500

    Create calculadora.h

---
+++ calculadora.h
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class Calculadora{
+private:
+      numero primero, segundo, modulo;
+
+public:      
+      int suma();
+      int resta();
+      int multiplicar();
+      
+      Calculadora(int a, int b, int mod){
+      
+                      numero.setprimero(a);
+                      numero.setsegundo(b);
+                      numero.setmodulo(mod);
+};
