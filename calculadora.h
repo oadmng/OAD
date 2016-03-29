e6283e56f14fc2fd55931bec738ecd4683f9a598
oadmng <oadmng@gmail.com>
2016-03-29 07:46:46 -0500

    Update calculadora.h

--- calculadora.h
+++ calculadora.h
@@ -3,16 +3,22 @@ using namespace std;
 
 class Calculadora
 {
+
 public:
+int respuesta;
+
 int suma(int a, int b,int mod){
     return (a+b)%mod;
     }
 int resta(int a, int b,int mod){
-
-    return(a-b)%mod;
+ respuesta=(a-b)%mod;
+if (respuesta <0){respuesta=respuesta+mod;}
+    return respuesta;
     }
 int multiplicar(int a, int b, int mod){
-    return (a*b)%mod;
+     respuesta=(a*b)%mod;
+if (respuesta <0){respuesta=respuesta+mod;}
+    return respuesta;
     }
 
 };
